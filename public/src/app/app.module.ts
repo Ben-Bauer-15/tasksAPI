b78cf7084bb1d6f10664109c84e85c98ad043b04
Ben Bauer <benjaminbauer15@gmail.com>
2018-12-04 11:56:22 -0800

    added new component tasks

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpService } from './http.service'
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { TasksComponent } from './tasks/tasks.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
